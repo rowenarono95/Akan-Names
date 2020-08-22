1ff8749a67faa5826d6be619c4d8ab822e60e786
rowenarono95 <rowenarono@gmail.com>
2020-08-22 16:15:54 +0300

    added dateOfBirth function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+ 
+
+function dateOfBirth(){
+   var days=document.getElementById("day").value;
+   var birthDate = new Date(days);
+   var dayOfBirth= birthDate.getDay();
+   //get gender
+   var gender=document.getElementsByName("gender").value;
+}
